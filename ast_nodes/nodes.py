75b2b462cef5d38c7deace2cf597adbb3f12b94f
Priyanshu <priyanshu.codejee.24@kgpian.iitkgp.ac.in>
2026-04-06 14:45:06 +0530

    nodes classes created, parser made uto rule 5

--- ast_nodes/nodes.py
+++ ast_nodes/nodes.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
-from typing import List, Union
+from dataclasses import dataclass, field
+from typing import List, Optional, Union
 
-from dataclass import dataclass
+from lexer import TokenType
 
 
 @dataclass
@@ -86,3 +87,308 @@ Expr = Union[
     BinOp,
     UnaryOp,
 ]
+
+
+@dataclass
+class CmpExpr:
+    op: str
+    left: Expr
+    right: Expr
+
+
+@dataclass
+class GeometricPred:
+    subject: str
+    relation: str
+    target: str
+
+
+BoolExpr = Union[CmpExpr, GeometricPred]
+
+
+@dataclass
+class PropAssign:
+    name: str
+    value: Expr
+
+
+@dataclass
+class PosConstraint:
+    kind: str
+    targets: List[Expr]
+
+
+@dataclass
+class RelConstraint:
+    kind: str
+    target: str
+
+
+ConstraintClause = Union[PosConstraint, RelConstraint]
+
+
+@dataclass
+class PrimitiveDecl:
+    shape_kw: TokenType  # prim_kw
+    name: str  # ident
+    props: List[PropAssign]
+    args: List[str] = field(default_factory=list)  # of INdent {IDENT}
+    constraints: List[ConstraintClause] = field(default_factory=list)
+    constraint: Optional[ConstraintClause] = None
+
+
+@dataclass
+class DerivedDecl:
+    kind: str
+    name: Optional[str]
+    args: List[str]  #
+    locus_var: Optional[str] = field(default=None)
+    locus_constraint: Optional[BoolExpr] = field(default=None)
+
+
+ShapeDecl = Union[PrimitiveDecl, DerivedDecl]
+
+
+@dataclass
+class ConstraintStmt:
+    subject: str  # the ident
+    constraint: ConstraintClause  # pos or rel
+
+
+ConstraintStmtGroup = Union[ConstraintStmt]
+
+
+@dataclass
+class DistanceMeasure:
+    point_a: str
+    point_b: str
+    value: Expr
+
+
+@dataclass
+class AngleMeasure:
+    angle_name: str
+    value: AngleLiteral
+
+
+@dataclass
+class LengthMeasure:
+    shape_name: str
+    value: Expr
+
+
+@dataclass
+class RadiusMeasure:
+    shape_name: str
+    value: Expr
+
+
+@dataclass
+class RatioMeasure:
+    shape_a: str
+    shape_b: str
+    value: RatioLiteral
+
+
+@dataclass
+class AreaMeasure:
+    shape_name: str
+    value: Expr
+
+
+@dataclass
+class PerimeterMeasure:
+    shape_name: str
+    value: Expr
+
+
+MeasureStmt = Union[
+    DistanceMeasure,
+    AngleMeasure,
+    LengthMeasure,
+    RadiusMeasure,
+    RatioMeasure,
+    AreaMeasure,
+    PerimeterMeasure,
+]
+
+
+@dataclass
+class LetStmt:
+    name: str
+    value: Expr
+
+
+@dataclass
+class SetStmt:
+    name: str
+    value: Expr
+
+
+@dataclass
+class RangeSpec:
+    start: Expr
+    end: Expr
+    step: Expr
+
+
+@dataclass
+class ParamStmt:
+    name: str
+    range_spec: RangeSpec
+
+
+VarStmt = Union[LetStmt, SetStmt, ParamStmt]
+
+
+@dataclass
+class SweepStmt:
+    params: List[str]
+    speed: Optional[Expr]
+
+
+@dataclass
+class ElseIfClause:
+    condition: BoolExpr
+    body: "List[Statement]"
+
+
+@dataclass
+class IfStmt:
+    condition: BoolExpr
+    body: "List[Statement]"
+    else_ifs: List[ElseIfClause]
+    else_body: "Optional[List[Statement]]"
+
+
+@dataclass
+class ForStmt:
+    var: str
+    values: List[Expr]
+    body: "List[Statement]"
+
+
+ControlStmt = Union[SweepStmt, IfStmt, ForStmt]
+
+
+@dataclass
+class ReturnStmt:
+    value: Optional[Expr]
+
+
+@dataclass
+class DefineStmt:
+    name: str
+    params: List[str]
+    body: "List[Statement]"
+
+
+@dataclass
+class CallStmt:
+    name: str
+    args: List[Expr]
+
+
+FunctionStmt = Union[DefineStmt, CallStmt]
+
+
+@dataclass
+class ReflectStmt:
+    target: str
+    over: str
+
+
+@dataclass
+class RotateStmt:
+    target: str
+    by: AngleLiteral
+    about: str
+
+
+@dataclass
+class ScaleStmt:
+    target: str
+    by: Expr
+
+
+@dataclass
+class TranslateStmt:
+    target: str
+    by: VectorExpr
+
+
+TransformStmt = Union[ReflectStmt, RotateStmt, ScaleStmt, TranslateStmt]
+
+
+@dataclass
+class AssertStmt:
+    expr: BoolExpr
+
+
+AssertStmtGroup = Union[AssertStmt]
+
+
+@dataclass
+class LabelStmt:
+    target: str
+    text: str
+
+
+@dataclass
+class NoteStmt:
+    text: str
+
+
+@dataclass
+class HideStmt:
+    targets: List[str]
+
+
+@dataclass
+class GridStmt:
+    pass
+
+
+RenderStmt = Union[
+    LabelStmt,
+    NoteStmt,
+    HideStmt,
+    GridStmt,
+]
+
+
+Statement = Union[
+    PrimitiveDecl,
+    DerivedDecl,
+    ConstraintStmt,
+    DistanceMeasure,
+    AngleMeasure,
+    LengthMeasure,
+    RadiusMeasure,
+    RatioMeasure,
+    AreaMeasure,
+    PerimeterMeasure,
+    LetStmt,
+    SetStmt,
+    ParamStmt,
+    SweepStmt,
+    IfStmt,
+    ForStmt,
+    DefineStmt,
+    CallStmt,
+    ReflectStmt,
+    RotateStmt,
+    ScaleStmt,
+    TranslateStmt,
+    AssertStmt,
+    LabelStmt,
+    NoteStmt,
+    HideStmt,
+    GridStmt,
+    MeasureStmt,
+    ReturnStmt,
+]
+
+
+class Program:
+    statements: List[Statement]
--- parser/parser.py
+++ parser/parser.py
@@ -2,7 +2,157 @@ from __future__ import annotations
 
 from typing import List
 
-from lexer import Token, TokenType
+from ast_nodes.nodes import (
+    AngleLiteral,
+    BinOp,
+    BoolExpr,
+    # expressions
+    CallExpr,
+    CmpExpr,
+    ConstraintClause,
+    # constraint stmt
+    ConstraintStmt,
+    DerivedDecl,
+    # measure stmts
+    Expr,
+    GeometricPred,
+    IdentExpr,
+    NumberLiteral,
+    PosConstraint,
+    # shape declarations
+    PrimitiveDecl,
+    Program,
+    # building blocks
+    PropAssign,
+    RatioLiteral,
+    RelConstraint,
+    # function stmts
+    Statement,
+    StringLiteral,
+    # control stmts
+    UnaryOp,
+    VectorExpr,
+)
+from lexer import Token
+from lexer.token_types import TokenType
+
+from .parser_utils import ParseError, advance, expect, peek
+
+_PRIM_KWS = frozenset(
+    {
+        TokenType.POINT,
+        TokenType.SEGMENT,
+        TokenType.LINE,
+        TokenType.RAY,
+        TokenType.CIRCLE,
+        TokenType.ARC,
+        TokenType.TRIANGLE,
+        TokenType.RECTANGLE,
+        TokenType.RHOMBUS,
+        TokenType.REGULAR_POLY,
+        TokenType.POLYGON,
+        TokenType.ELLIPSE,
+        TokenType.PARALLELOGRAM,
+    }
+)
+
+_DERIVED_KWS = frozenset(
+    {
+        TokenType.MIDPOINT,
+        TokenType.INTERSECTION,
+        TokenType.PERPENDICULAR_BISECTOR,
+        TokenType.ANGLE_BISECTOR,
+        TokenType.CIRCUMCIRCLE,
+        TokenType.INCIRCLE,
+        TokenType.CONVEX_HULL,
+        TokenType.LOCUS,
+    }
+)
+
+_PROP_NAMES = frozenset(
+    {
+        TokenType.RADIUS,
+        TokenType.LENGTH,
+        TokenType.ANGLE,
+        TokenType.AREA,
+        TokenType.PERIMETER,
+        TokenType.SIDES,
+    }
+)
+
+_REL_CONSTRAINT_KWS = frozenset(
+    {
+        TokenType.PARALLEL_TO,
+        TokenType.PERPENDICULAR_TO,
+        TokenType.TANGENT_TO,
+        TokenType.BISECTS,
+    }
+)
+
+_POS_CONSTRAINT_KWS = frozenset(
+    {
+        TokenType.AT,
+        TokenType.ON,
+        TokenType.PASSES_THROUGH,
+        TokenType.CENTERED_AT,
+    }
+)
+
+_CONSTRAINT_KWS = _REL_CONSTRAINT_KWS | _POS_CONSTRAINT_KWS
+
+_CMP_OPS = frozenset(
+    {
+        TokenType.EQ,
+        TokenType.NEQ,
+        TokenType.LT,
+        TokenType.GT,
+        TokenType.LTE,
+        TokenType.GTE,
+    }
+)
+
+_GEO_PRED_KWS = frozenset(
+    {
+        TokenType.ON,
+        TokenType.PARALLEL_TO,
+        TokenType.PERPENDICULAR_TO,
+        TokenType.TANGENT_TO,
+        TokenType.BISECTS,
+        TokenType.PASSES_THROUGH,
+        TokenType.CENTERED_AT,
+    }
+)
+
+_MEASURE_KWS = frozenset(
+    {
+        TokenType.DISTANCE,
+        TokenType.ANGLE,
+        TokenType.LENGTH,
+        TokenType.RADIUS,
+        TokenType.RATIO,
+        TokenType.AREA,
+        TokenType.PERIMETER,
+    }
+)
+
+_TRANSFORM_KWS = frozenset(
+    {
+        TokenType.REFLECT,
+        TokenType.ROTATE,
+        TokenType.SCALE,
+        TokenType.TRANSLATE,
+    }
+)
+
+_RENDER_KWS = frozenset(
+    {
+        TokenType.LABEL,
+        TokenType.NOTE,
+        TokenType.HIDE,
+        TokenType.GRID,
+        TokenType.MEASURE,
+    }
+)
 
 
 class Parser:
@@ -10,6 +160,265 @@ class Parser:
         self._tokens: List[Token] = tokens
         self._pos: int = 0
         self._skip_newlines()
+
+    def parse(self) -> Program:
+        statements: List[Statement] = []
+        while self.cur().type != TokenType.EOF:
+            statements.append(self._parse_statement())
+            self._skip_newlines
+        return Program(statements=statements)
+
+    # RULE 1 of grammar
+
+    def _parse_statement(self) -> Statement:
+        tok = self._cur()
+        tt = tok.type()
+
+        if tt in _PRIM_KWS:
+            return self.parse_primitive_decl()
+
+    # RULE 2 - Primitive decl
+
+    def _parse_primitive_decl(self) -> PrimitiveDecl:
+        kw_tok, self._pos = advance(self._tokens, self._pos)
+        name_tok, self._pos = expect(self._tokens, self._pos, TokenType.IDENT)
+
+        args: List[str] = []
+        props: List[PropAssign] = []
+        constraints: List[ConstraintClause] = []
+
+        if self._cur().type == TokenType.OF:
+            self._pos += 1
+            first_arg, self._pos = expect(self._tokens, self._pos, TokenType.IDENT)
+            args.append(first_arg.value)
+            while self._cur().type == TokenType.IDENT:
+                next_arg, self._pos = advance(self._tokens, self._pos)
+                args.append(next_arg.value)
+
+        if self._cur().type == TokenType.WITH:
+            self._pos += 1
+            self._skip_newlines()
+            props.append(self._parse_prop_assign())
+            while self._cur().type == TokenType.COMMA:
+                self._pos += 1
+                self._skip_newlines()
+                props.append(self._parse_prop_assign())
+
+        while self._cur().type in _CONSTRAINT_KWS:
+            constraints.append(self._parse_constraint_clause())
+
+        return PrimitiveDecl(
+            shape_kw=kw_tok.type,
+            name=name_tok.value,
+            props=props,
+            args=args,
+            constraints=constraints,
+            constraint=constraints[0] if constraints else None,
+        )
+
+    def _parse_prop_assign(self) -> PropAssign:
+        prop_tok, self._pos = expect(
+            self._tokens,
+            self._pos,
+            TokenType.IDENT,
+            *tuple(_PROP_NAMES),
+        )
+        _, self._pos = expect(self._tokens, self._pos, TokenType.EQ)
+        value = self._parse_expr()
+        return PropAssign(name=prop_tok.value, value=value)
+
+    # RUle -3
+
+    def _parse_derived_decl(self) -> DerivedDecl:
+        kw_tok, self._pos = advance(self.tokens, self._pos)
+        kw = kw_tok.tpye
+
+        if kw == TokenType.LOCUS:
+            return self._parse_locus()
+
+        name_tok, self._pos = expect(self._tokens, self._pos, TokenType.IDENT)
+        _, self._pos = expect(self._tokens, self._pos, TokenType.OF)
+
+    def _parse_locus(self) -> DerivedDecl:
+        _, self._pos = expect(self._tokens, self._pos, TokenType.OF)
+        _, self._pos = expect(self._tokens, self._pos, TokenType.POINT)
+        var_tok, self._pos = expect(self._tokens, self._pos, TokenType.IDENT)
+        _, self._pos = expect(self._tokens, self._pos, TokenType.LBRACE)
+        self._skip_newlines()
+
+        kw_tok = self._cur()
+        if kw_tok.type != TokenType.IDENT or kw_tok.value != "constraint":
+            raise ParseError("Expected keyword 'constraint' inside locus body", kw_tok)
+        self._pos += 1
+
+        bool_ex = self._parse_bool_expr()
+        self._skip_newlines()
+        _, self._pos = expect(self._tokens, self._pos, TokenType.RBRACE)
+
+        return DerivedDecl(
+            kind="locus",
+            name=None,
+            args=[],
+            locus_var=var_tok.value,
+            locus_constraint=bool_ex,
+        )
+
+    # RULE - 4
+    def _parse_constraint_clause(self) -> ConstraintClause:
+        tt = self._cur().type
+        if tt in _POS_CONSTRAINT_KWS:
+            return self._parse_pos_constraint()
+        if tt in _REL_CONSTRAINT_KWS:
+            return self._parse_rel_constraint()
+        raise ParseError("Expected a constraint keyword", self._cur())
+
+    def _parse_pos_contraint(self) -> PosConstraint:
+        kw_tok, self._pos = advance(self._tokens, self._pos)
+        kw = kw_tok.type
+
+        if kw == TokenType.AT:
+            expr = self._parse_expr()
+            return PosConstraint(kind="at", targets=[expr])
+
+        if kw == TokenType.ON:
+            name_tok, self._pos = expect(self._tokens, self._pos, TokenType.IDENT)
+            return PosConstraint(kind="on", targets=[IdentExpr(name_tok.value)])
+
+        if kw == TokenType.PASSES_THROUGH:
+            first_tok, self._pos = expect(self._tokens, self._pos, TokenType.IDENT)
+            targets: List[Expr] = [IdentExpr(first_tok.value)]
+            while self._cur().type == TokenType.IDENT:
+                more_tok, self._pos = advance(self._tokens, self._pos)
+                targets.append(IdentExpr(more_tok.value))
+            return PosConstraint(kind="passes_through", targets=targets)
+
+        if kw == TokenType.CENTERED_AT:
+            name_tok, self._pos = expect(self._tokens, self._pos, TokenType.IDENT)
+            return PosConstraint(
+                kind="centered_at", targets=[IdentExpr(name_tok.value)]
+            )
+
+        raise ParseError("Unrecognised positional constraint", kw_tok)
+
+    def _parse_rel_constraint(self) -> RelConstraint:
+        kw_tok, self._pos = advance(self._tokens, self._pos)
+        name_tok, self._pos = expect(self._tokens, self._pos, TokenType.IDENT)
+        return RelConstraint(kind=kw_tok.value, target=name_tok.value)
+
+    # --------------------------------------
+    # RULE 5
+
+    def _parse_constraint_stmt(self) -> ConstraintStmt:
+        subj_tok, self._pos = advance(self._tokens, self._pos)
+        constraint = self._parse_constraint_clause()
+        return ConstraintStmt(subject=subj_tok.value, constraint=constraint)
+
+    # RULE -13
+
+    def _parse_expr(self) -> Expr:
+        return self._parse_add_expr()
+
+    def _parse_add_expr(self) -> Expr:
+        left = self._parse_mul_expr()
+        while self._cur().type in (TokenType.PLUS, TokenType.MINUS):
+            op_tok, self._pos = advance(self._tokens, self._pos)
+            right = self._parse_mul_expr()
+            left = BinOp(op=op_tok.value, left=left, right=right)
+        return left
+
+    def _parse_mul_expr(self) -> Expr:
+        left = self._parse_unary()
+        while self._cur().type in (TokenType.STAR, TokenType.SLASH):
+            op_tok, self._pos = advance(self._tokens, self._pos)
+            right = self._parse_unary()
+            left = BinOp(op=op_tok.value, left=left, right=right)
+        return left
+
+    def _parse_unary(self) -> Expr:
+        if self._cur().type == TokenType.MINUS:
+            self._pos += 1
+            return UnaryOp(op="-", operand=self._parse_primary())
+        return self._parse_primary()
+
+    def _parse_primary(self) -> Expr:
+        tok = self._cur()
+        tt = tok.type
+
+        if tt == TokenType.NUMBER:
+            self._pos += 1
+            val = float(tok.value)
+
+            if self._cur().type == TokenType.DEG:
+                self._pos += 1
+                return AngleLiteral(degrees=val)
+
+            if self._cur().type == TokenType.COLON:
+                self._pos += 1
+                rhs_tok, self._pos = expect(self._tokens, self._pos, TokenType.NUMBER)
+                return RatioLiteral(left=val, right=float(rhs_tok.value))
+
+            return NumberLiteral(value=val)
+
+        if tt == TokenType.STRING:
+            self._pos += 1
+            return StringLiteral(value=tok.value)
+
+        if tt in (TokenType.IDENT, TokenType.DISTANCE):
+            name = tok.value
+            self._pos += 1
+            if self._cur().type == TokenType.LPAREN:
+                self._pos += 1
+                args: List[Expr] = []
+                if self._cur().type != TokenType.RPAREN:
+                    args.append(self._parse_expr())
+                    while self._cur().type == TokenType.COMMA:
+                        self._pos += 1
+                        args.append(self._parse_expr())
+                _, self._pos = expect(self._tokens, self._pos, TokenType.RPAREN)
+                return CallExpr(name=name, args=args)
+
+            if tt == TokenType.IDENT:
+                return IdentExpr(name=name)
+            raise ParseError("Expected a function call", tok)
+
+        if tt == TokenType.LPAREN:
+            self._pos += 1  # consume '('
+            first = self._parse_expr()
+
+            if self._cur().type == TokenType.COMMA:
+                self._pos += 1
+                second = self._parse_expr()
+                _, self._pos = expect(self._tokens, self._pos, TokenType.RPAREN)
+                return VectorExpr(x=first, y=second)
+
+            _, self._pos = expect(self._tokens, self._pos, TokenType.RPAREN)
+            return first  # grouped expr
+
+        raise ParseError("Expected a primary expression", tok)
+
+    # RUle -14
+    def _parse_bool_expr(self) -> BoolExpr:
+        if (
+            self._cur().type == TokenType.IDENT
+            and peek(self._tokens, self._pos, 1).type in _GEO_PRED_KWS
+        ):
+            subj_tok, self._pos = advance(self._tokens, self._pos)
+            rel_tok, self._pos = advance(self._tokens, self._pos)
+            tgt_tok, self._pos = expect(self._tokens, self._pos, TokenType.IDENT)
+            return GeometricPred(
+                subject=subj_tok.value,
+                relation=rel_tok.value,
+                target=tgt_tok.value,
+            )
+
+        left = self._parse_expr()
+        if self._cur().type not in _CMP_OPS:
+            raise ParseError(
+                "Expected a comparison operator in boolean expression", self._cur()
+            )
+        op_tok, self._pos = advance(self._tokens, self._pos)
+        right = self._parse_expr()
+        return CmpExpr(op=op_tok.value, left=left, right=right)
 
     # Helper Functions
 
